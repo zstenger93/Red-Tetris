06d96fddb07cbe7a015119b6db1a959729f57d25
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-06-15 17:57:44 +0200

    button opacity

--- app/src/client/HomePage.vue
+++ app/src/client/HomePage.vue
@@ -6,27 +6,29 @@
         </button>
         <div v-if="showOptions && !showMultiplayerOptions" class="flex flex-col items-center justify-center space-y-4">
             <button @click="selectOption(1)"
-                class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Solo Tetris
             </button>
             <button @click="selectOption(2)"
-                class="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                class="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Multiplayer
             </button>
             <button @click="selectOption(3)"
-                class="bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                class="bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Back
             </button>
         </div>
         <div v-if="showMultiplayerOptions" class="flex flex-col items-center justify-center space-y-4">
-            <button class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+            <button
+                class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Tetris
             </button>
-            <button class="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+            <button
+                class="bg-green-800 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Tic-Tac-Toe
             </button>
             <button @click="backToMainOptions"
-                class="bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                class="bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer bg-opacity-50">
                 Back
             </button>
         </div>
