bf5eedfd1379d6936b3eab3ea9ede734c9e11e62
Zsolt Stenger <zstenger@student.42heilbronn.de>
2024-06-23 10:03:28 +0200

    how to play part button fix

--- app/src/components/HowToPlay.vue
+++ app/src/components/HowToPlay.vue
@@ -1,20 +1,23 @@
 <template>
-    <div class="flex justify-center items-center h-screen overflow-auto">
-        <div class="flex bg-gray-900 p-4 rounded-lg"> <!-- Added padding and rounded corners for better visual -->
+    <div class="flex justify-center items-center overflow-auto">
+        <div class="flex bg-gray-900 bg-opacity-80 p-4 rounded-lg"> <!-- Added padding and rounded corners for better visual -->
             <div class="flex flex-col justify-center mr-4">
-                <div class="flex items-center mb-2 text-white font-bold">
+				<div class="flex items-center justify-center mb-2 text-white font-bold">
+                    <span>TETRIS</span>
+                </div>
+                <div class="flex items-center justify-center mb-2 text-white font-bold">
                     <font-awesome-icon :icon="['fas', 'angle-left']" class="text-white mr-6" />
                     <span>Left</span>
                 </div>
-                <div class="flex items-center mb-2 text-white font-bold">
+                <div class="flex items-center justify-center mb-2 text-white font-bold">
                     <font-awesome-icon :icon="['fas', 'angle-right']" class="text-white mr-6" />
                     <span>Right</span>
                 </div>
-                <div class="flex items-center mb-2 text-white font-bold">
+                <div class="flex items-center justify-center mb-2 text-white font-bold">
                     <font-awesome-icon :icon="['fas', 'angle-up']" class="text-white mr-5" />
                     <span>Rotation</span>
                 </div>
-                <div class="flex items-center mb-2 text-white font-bold">
+                <div class="flex items-center justify-center mb-2 text-white font-bold">
                     <font-awesome-icon :icon="['fas', 'angle-down']" class="text-white mr-5" />
                     <span>Fall towards the pile</span>
                 </div>
