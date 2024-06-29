a94a7295f2be798b3887b099ec74d3274551f09c
kvebers <kvebers@student.42heilbronn.de>
2024-06-29 08:46:04 +0200

    Added lobby selection boxes

---
+++ app/src/client/TetrisLobby.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex-shrink-0 flex-grow-0" style="width: 30rem; height: 80vh;">
+        <div>
+            <input type="text" placeholder="Enter your name" class="border-2 rounded border-white border-opacity-50 bg-gray-900 bg-opacity-80 text-white p-2">
+            <input type="text" placeholder="Enter your room code" class="border-2 rounded border-white border-opacity-50 bg-gray-900 bg-opacity-80 text-white p-2">
+            <button class="border-2 rounded border-white border-opacity-50 bg-gray-900 bg-opacity-80 text-white p-2">Join Room</button>
+        </div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    name: 'TetrisLobby',
+}
+</script>
