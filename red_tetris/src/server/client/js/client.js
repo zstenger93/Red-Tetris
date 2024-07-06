a32f329157a361d9f82af760440f7d0407b5353b
kvebers <kvebers@student.42heilbronn.de>
2024-07-06 10:56:43 +0200

    Cleanup of the file

--- red_tetris/src/server/client/js/client.js
+++ red_tetris/src/server/client/js/client.js
@@ -2,37 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const homeDiv = document.getElementById('home');
     const gameDiv = document.getElementById('game');
     const joinButton = document.getElementById('join');
-    const sendMessageButton = document.getElementById('sendMessage');
-    const roomInfoDiv = document.getElementById('roomInfo');
-    const messagesDiv = document.getElementById('messages');
-    const messageInput = document.getElementById('messageInput');
 
     let socket;
 
     function navigateToGame(username, room) {
         homeDiv.classList.add('hidden');
         gameDiv.classList.remove('hidden');
-        roomInfoDiv.textContent = `Room: ${room}, Player: ${username}`;
-        
         socket = io('http://localhost:8080');
         socket.on('connect', () => {
             console.log('Connected to the WebSocket server');
             socket.emit('joinRoom', {room, username});
-        });
-        
-        socket.on('message', (message) => {
-            console.log('Received message:', message);
-            const messageElement = document.createElement('div');
-            messageElement.textContent = message;
-            messagesDiv.appendChild(messageElement);
-        });
-
-        sendMessageButton.addEventListener('click', () => {
-            const message = messageInput.value;
-            if (message) {
-                socket.emit('message', { room, message });
-                messageInput.value = '';
-            }
         });
     }
 
@@ -43,8 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (username && room) {
             history.pushState(null, '', `/#${room}[${username}]`);
             navigateToGame(username, room);
-        } else {
-            alert('Please enter both username and room.');
+        }
+        else {
+            console.log("Please enter a username and room")
         }
     });
 
