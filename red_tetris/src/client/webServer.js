267555f4a33cea6157274af96e161a6b2b2e0e56
kvebers <kvebers@student.42heilbronn.de>
2024-07-11 17:27:30 +0200

    Seperate frontend from backend

--- red_tetris/src/client/webServer.js
+++ red_tetris/src/client/webServer.js
@@ -6,8 +6,21 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
 
+// Serve the JS file
+app.get('/js/client.js', (req, res) => {
+    res.sendFile(path.join(__dirname,'js', 'client.js'));
+    console.log('client.js sent');
+});
+
+
+
+app.get('/css/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'css', 'style.css'));
+});
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const httpServer = http.createServer(app);
