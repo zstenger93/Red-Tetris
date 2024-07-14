49cfab05e723fbf6b7cd718f082383d977a5126d
kvebers <kvebers@student.42heilbronn.de>
2024-07-14 14:07:57 +0200

    Update: Game Loop Working

--- red_tetris/src/server/classes/Player.js
+++ red_tetris/src/server/classes/Player.js
@@ -1,13 +1,14 @@
 class Player {
-    constructor(name, socketId) {
-        this.name = name;
-        this.socketId = socketId;
-        this.score = 0;
-        this.board = Array.from({ length: 20 }, () => Array(10).fill(0));
-        this.currentPiece = null;
-        this.nextPiece = null;
-        this.isAlive = true;
-    }
+  constructor(name, socketId) {
+    this.name = name;
+    this.socketId = socketId;
+    this.score = 0;
+    this.board = Array.from({ length: 20 }, () => Array(10).fill(0));
+    this.currentPiece = null;
+    this.nextPiece = null;
+    this.isAlive = true;
+    this.currentPiece = null;
+  }
 }
 
 module.exports = Player;
