0e7c82c3a8709fa39772da5e451b52b25d56bb38
kvebers <kvebers@student.42heilbronn.de>
2024-07-20 09:00:41 +0200

    Add: Class pieces with inheritence add Board rendering mode

--- red_tetris/src/server/classes/Piece.js
+++ red_tetris/src/server/classes/Piece.js
@@ -1,7 +1,130 @@
 class Piece {
-    constructor(name, color) {
-        this.name = name;
-        this.color = color;
-        this.rotation = 0;
-    }
+  constructor() {
+    this.name = 0;
+    this.rotation = 0;
+    this.shape = [];
+  }
+
+  rotate() {
+    this.rotation = (this.rotation + 1) % 4;
+    this.shape = this.shape.map((row, i) =>
+      row.map((val, j) => this.shape[j][3 - i])
+    );
+  }
+
+  reverseRotate() {
+    this.rotation = (this.rotation - 1) % 4;
+    this.shape = this.shape.map((row, i) =>
+      row.map((val, j) => this.shape[3 - j][i])
+    );
+  }
+
+  debug() {
+    console.log(this.shape);
+  }
+
+  getShape() {
+    return this.shape;
+  }
 }
+
+class LPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 1;
+    this.rotation = 0;
+    this.shape = [
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 0, 0],
+    ];
+  }
+}
+
+class JPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 2;
+    this.rotation = 0;
+    this.shape = [
+      [1, 0, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ];
+  }
+}
+
+class ZPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 3;
+    this.rotation = 0;
+    this.shape = [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ];
+  }
+}
+
+class SPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 4;
+    this.rotation = 0;
+    this.shape = [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ];
+  }
+}
+
+class OPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 5;
+    this.rotation = 0;
+    this.shape = [
+      [1, 1],
+      [1, 1],
+    ];
+  }
+}
+
+class TPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 6;
+    this.rotation = 0;
+    this.shape = [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ];
+  }
+}
+
+class IPiece extends Piece {
+  constructor() {
+    super();
+    this.name = 7;
+    this.rotation = 0;
+    this.shape = [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+  }
+}
+
+module.exports = {
+  LPiece,
+  JPiece,
+  ZPiece,
+  SPiece,
+  OPiece,
+  TPiece,
+  IPiece,
+};
