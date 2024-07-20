66a646399077eec671f2d92955e5310828e6b95a
kvebers <kvebers@student.42heilbronn.de>
2024-07-20 09:00:41 +0200

    Add: Class pieces with inheritence add Board rendering mode

--- red_tetris/src/server/classes/Player.js
+++ red_tetris/src/server/classes/Player.js
@@ -1,4 +1,30 @@
-class Player {
+Piece = required("./Piece");
+
+collumnNames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+rowNames = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+];
+
+rowNames = class Player {
   constructor(name, socketId) {
     this.name = name;
     this.socketId = socketId;
@@ -8,7 +34,25 @@ class Player {
     this.nextPiece = null;
     this.isAlive = true;
     this.currentPiece = null;
+    this.gameBoard = [];
   }
-}
+
+  initGameBoard() {
+    this.gameBoard = Array.from({ length: 20 }, () => Array(10).fill(0));
+  }
+
+  addScore(score) {
+    this.score += score;
+  }
+
+  returnBoard() {
+    let returnMessage = "";
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board[i].length; j++) {
+        returnMessage += this.board[i][j] + rowNames[i] + collumnNames[j];
+      }
+    }
+  }
+};
 
 module.exports = Player;
